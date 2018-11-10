ca3383dbf36af74069b2ba5b8c896825e4fb8975
Elena741 <solo741@yandex.ru>
2018-11-10 16:22:37 +0300

    Обработчик добавления таблиц

---
+++ actionCreateTable.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+if ($_SESSION['login']!=='Елена' || $_SESSION['password']!=='789') {
+header("Location:tableList.php");
+exit();
+}
+require('connect.php');
+
+if (!empty($_POST['description'])) {
+	$description = $_POST['description'];
+
+foreach (array_slice($_POST, 1) as $value) {
+if (!empty($value)) {
+	$str .= "$value, ";
+}
+
+	
+
+}
+$connect = 	'CREATE TABLE '. $description.' (
+id int NOT NULL AUTO_INCREMENT, ';
+$connect .= $str;
+$connect .= 'PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;';
+
+	$sth = $pdo->prepare("$connect");
+    $sth->execute();
+
+}
+header("Location:tableList.php");
+
+?>
